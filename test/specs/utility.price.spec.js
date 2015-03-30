592a89f1c33152c0bbaee0db6003efa357794325
noel(이지만) <ace4gi@coupang.com>
2015-03-30 13:12:31 +0900

    utility.price.spec.js, utility.uiEnhancement.spec.js 스펙추가

--- test/specs/utility.price.spec.js
+++ test/specs/utility.price.spec.js
@@ -11,14 +11,32 @@ define([
 ],function ($, _, Handlebars, utility, helper){
 
     describe('utility.price.addComma', function(){
-        it('??????????????????????????????', function() {
+        it('결과값 테스트', function() {
+            expect(utility.price.addComma(12345)).toBe('12,345');
+            expect(utility.price.addComma('12345')).toBe('12,345');
+            
+            expect(utility.price.addComma(-12345)).toBe('-12,345');
+            expect(utility.price.addComma('-12345')).toBe('-12,345');
+        });
 
+        it('결과값 typeof 는 string 입니다.', function() {
+            expect(typeof utility.price.addComma(12345)).toBe('string');
+            expect(typeof utility.price.addComma('12345')).toBe('string');
+
+            expect(typeof utility.price.addComma(-12345)).toBe('string');
+            expect(typeof utility.price.addComma('-12345')).toBe('string');
         });
     });
 
     describe('utility.price.removeComma', function(){
-        it('??????????????????????????????', function() {
+        it('결과값 테스트', function() {
+            expect(utility.price.removeComma('12,345')).toBe(12345);
+            expect(utility.price.removeComma('-12,345')).toBe(-12345);
+        });
 
+        it('결과값 typeof 는 Number 입니다.', function() {
+            expect(typeof utility.price.removeComma('-12345')).toBe('number');
+            expect(typeof utility.price.removeComma('-12,345')).toBe('number');
         });
     });
     
--- test/specs/utility.uiEnhancements.spec.js
+++ test/specs/utility.uiEnhancements.spec.js
@@ -14,9 +14,21 @@ define([
         beforeEach(function(){
             loadFixtures('utility.uiEnhancements.spec.html');
         });
-
+        
         it('var ui={} 형태로 사용', function() {
-            // 사용법1
+            var $element = $('#uiEnhancements1');
+            var ui = {
+                test1 : '.test1',
+                test2 : '.test2',
+                radioChecked : ':radio[name=type]:checked'
+            };
+            utility.uiEnhancements.call(ui, $element);
+            
+            expect(ui.test1.text()).toBe('text1-1-txt 입니다.');
+            expect(ui.test2.text()).toBe('text1-2-txt 입니다.');
+        });
+
+        it('var ui={}에 적용된 uiEnhancements 으로 dom 다시 갱신', function() {
             var $element = $('#uiEnhancements1');
             var ui = {
                 test1 : '.test1',
@@ -25,12 +37,11 @@ define([
             };
             utility.uiEnhancements.call(ui, $element);
 
-            expect(ui.test1.text()).toBe('text1-1-txt 입니다.');
-            expect(ui.test2.text()).toBe('text1-2-txt 입니다.');
-
             expect(ui.radioChecked.attr("id")).toBe('cart1');
+            
             $("#cart1").removeProp('checked');
             $("#order1").prop('checked', true);
+            
             utility.uiEnhancements.call(ui, $element);
             expect(ui.radioChecked.attr("id")).toBe('order1');
         });
@@ -52,10 +63,28 @@ define([
 
             expect(object.ui.test1.text()).toBe("text2-1-txt 입니다.");
             expect(object.ui.test2.text()).toBe("text2-2-txt 입니다.");
+        });
+
+        it("var object={ ui:{} } 형태로 사용", function() {
+            // 사용법2
+            var object = {
+                initialize: function(){
+                    utility.uiEnhancements.call(this);
+                },
+                element : "#uiEnhancements2",
+                ui : {
+                    test1 : ".test1",
+                    test2 : ".test2",
+                    radioChecked : ':radio[name=type]:checked'
+                }
+            };
+            object.initialize();
 
             expect(object.ui.radioChecked.attr("id")).toBe('cart2');
+            
             $("#cart2").removeProp('checked');
             $("#order2").prop('checked', true);
+            
             utility.uiEnhancements.call(object);
             expect(object.ui.radioChecked.attr("id")).toBe('order2');
         });
